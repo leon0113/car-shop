ba40f9b84706343e495da0c9f7c7835fa0d145d8
leon0113 <tahjib.leon399@gmail.com>
2023-04-28 23:34:06 +0600

    done

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,7 +3,7 @@ import './Header.css'
 const Header = () => {
     return (
         <div className='header'>
-            <h1>Car Heaven</h1>
+            <h1>Car Shop</h1>
             <h3>Choose Your Best car </h3>
         </div>
     );
