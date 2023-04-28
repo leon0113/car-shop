9803bc95f2c7009fb1e51ba095da0a6a1bf16317
leon0113 <tahjib.leon399@gmail.com>
2023-04-28 23:33:17 +0600

    first commit

---
+++ src/components/Header/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Header.css'
+const Header = () => {
+    return (
+        <div className='header'>
+            <h1>Car Heaven</h1>
+            <h3>Choose Your Best car </h3>
+        </div>
+    );
+};
+
+export default Header;
